f03483715a60dd1c7e12bb3a3dc4f8a12bc2d40f
Érico Rolim <erico.erc@gmail.com>
2020-08-16 15:12:45 -0300

    svlogd.c: Initialize all fields of sockaddr_in.
    http://skarnet.org/cgi-bin/archive.cgi?2:mss:1163:201602:gpiglpbjdemlioaeabbn

--- src/svlogd.c
+++ src/svlogd.c
@@ -430,6 +430,7 @@ unsigned int logdir_open(struct logdir *ld, const char *fn) {
   ld->name =(char*)fn;
   ld->ppid =0;
   ld->match ='+';
+  ld->udpaddr.sin_family =AF_INET;
   ld->udpaddr.sin_port =0;
   ld->udponly =0;
   while (! stralloc_copys(&ld->prefix, "")) pause_nomem();
